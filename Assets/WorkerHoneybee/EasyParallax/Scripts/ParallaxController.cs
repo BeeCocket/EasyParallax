b9944b89b9d6f9483cb4eff75a608e32dfbda118
WorkerHoneybee <jarek.kania2@gmail.com>
2020-03-17 22:30:41 +0100

    Unity version changed - Cleaning - Simplification - In editor preview

---
+++ Assets/WorkerHoneybee/EasyParallax/Scripts/ParallaxController.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using UnityEngine;
+
+namespace WorkerHoneybee.EasyParallax
+{
+    public class ParallaxController : MonoBehaviour
+    {
+        public event Action<Vector2> OnGyroShift;
+        public static ParallaxController Instance;
+
+
+        [Range(-1.0f, 1.0f)]
+        [field: SerializeField]
+        private float XShift;
+
+        [Range(-1.0f, 1.0f)]
+        [field: SerializeField]
+        private float YShift;
+
+        [field: SerializeField]
+        private float MaxShift;
+
+        private Gyroscope Gyroscope { get; set; }
+        private bool IsGyroEnabled { get; set; }
+
+        private Vector2 GyroShiftCache;
+
+        protected virtual void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else if (Instance != this)
+            {
+                Destroy(gameObject);
+            }
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+        protected virtual void OnEnable()
+        {
+            OnGyroShift = delegate { };
+            GyroShiftCache = Vector2.zero;
+#if UNITY_EDITOR
+            IsGyroEnabled = false;
+#else
+            IsGyroEnabled = EnableGyro();
+#endif
+        }
+
+        private bool EnableGyro()
+        {
+            if (SystemInfo.supportsGyroscope == true)
+            {
+                Gyroscope = Input.gyro;
+                Gyroscope.enabled = true;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        protected virtual void Update()
+        {
+#if UNITY_EDITOR
+            OnGyroShift(new Vector2(XShift * MaxShift, YShift * MaxShift));
+#else
+            if (IsGyroEnabled)
+            {
+                GyroShiftCache.x -= Gyroscope.rotationRateUnbiased.y;
+                GyroShiftCache.y += Gyroscope.rotationRateUnbiased.x;
+
+                GyroShiftCache.x = GyroShiftCache.x > MaxShift ? MaxShift : GyroShiftCache.x;
+                GyroShiftCache.y = GyroShiftCache.y > MaxShift ? MaxShift : GyroShiftCache.y;
+
+                GyroShiftCache.x = GyroShiftCache.x < -MaxShift ? -MaxShift : GyroShiftCache.x;
+                GyroShiftCache.y = GyroShiftCache.y < -MaxShift ? -MaxShift : GyroShiftCache.y;
+
+                OnGyroShift(GyroShiftCache);
+            }
+#endif
+        }
+    }
+}
---
+++ Assets/WorkerHoneybee/EasyParallax/Scripts/ParallaxLayer.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace WorkerHoneybee.EasyParallax
+{
+    public class ParallaxLayer : MonoBehaviour
+    {
+        [field: SerializeField]
+        private float SpeedFactor;
+
+        private RectTransform RectTransformComponent { get; set; }
+        private Vector2 DefaultPosition { get; set; }
+
+        protected virtual void Awake()
+        {
+            RectTransformComponent = GetComponent<RectTransform>();
+            DefaultPosition = RectTransformComponent.anchoredPosition;
+        }
+
+        protected virtual void Start()
+        {
+            ParallaxController.Instance.OnGyroShift -= ShiftLayer;
+            ParallaxController.Instance.OnGyroShift += ShiftLayer;
+        }
+
+        protected virtual void OnDestroy()
+        {
+            ParallaxController.Instance.OnGyroShift -= ShiftLayer;
+        }
+
+        public void ShiftLayer(Vector2 shift)
+        {
+            RectTransformComponent.anchoredPosition = DefaultPosition + (shift * SpeedFactor);
+        }
+    }
+}
